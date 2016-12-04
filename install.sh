9a07fe3e2b2a2940f2fb811f2f34e73df3fc3e17
qioixiy <qioixiy@gmail.com>
2016-12-04 22:14:37 +0800

    instal.sh for bash bootstrap

--- install.sh
+++ install.sh
@@ -7,3 +7,15 @@ for i in $list
 do
 	cp $i ~/ -rfv
 done
+
+# bash bootstrap
+grep profile_priv ~/.bashrc
+status=$?
+if [ 1 -eq $status ]
+then
+    echo make bash boot strap
+    cat <<_EOF >> ~/.bashrc
+# ~/.profile_priv
+[ -f ~/.profile_priv ] && . ~/.profile_priv
+_EOF
+fi
